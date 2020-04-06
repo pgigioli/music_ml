a939dc2bf684ec604815246e05c7e548e5558b9b
EC2 Default User <ec2-user@ip-172-16-107-52.ec2.internal>
2020-04-06 18:08:56 +0000

    faster librosa installation

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-conda install -c conda-forge librosa -y
+#conda install -c conda-forge librosa -y
+
+pip install librosa --user
+cd ../
+wget http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28.tar.gz
+tar -xzf libsndfile-1.0.28.tar.gz
+cd libsndfile-1.0.28
+./configure --prefix=/usr --disable-static --docdir=/usr/share/doc/libsndfile-1.0.28
+sudo make install
+rm ../libsndfile-1.0.28.tar.gz
