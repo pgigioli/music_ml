757d1a3735dd43670bcc14b0d54a5efd38fb3888
EC2 Default User <ec2-user@ip-172-16-96-76.ec2.internal>
2020-04-09 22:38:29 +0000

    wavenet embedding notebook

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#conda install -c conda-forge librosa -y
 
+# install librosa
 pip install librosa --user
+
+# manually install sndfile
 cd ../
 wget http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28.tar.gz
 tar -xzf libsndfile-1.0.28.tar.gz
@@ -10,5 +12,13 @@ cd libsndfile-1.0.28
 sudo make install
 cd ../
 rm libsndfile-1.0.28.tar.gz
+
+# install magenta
 sudo yum install alsa-lib-devel -y
 pip install magenta --user
+
+# download wavenet weights
+cd music_ml
+wget http://download.magenta.tensorflow.org/models/nsynth/wavenet-ckpt.tar
+tar -xvf wavenet-ckpt.tar
+rm wavenet-ckpt.tar
